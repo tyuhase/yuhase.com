52af7d13aba0e4d8f884b0add39e2de99739abc1
Tanner Yuhase <tyuhase@mail.wvu.edu>
2018-07-10 14:17:44 -0400

    portfolio section overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,47 @@ $(document).ready(function() {
     var randomNumber = Math.floor(Math.random() * classCycle.length);
     var classToAdd = classCycle[randomNumber];
 
-    $('header').addClass(classToAdd);
+    $('.hero').addClass(classToAdd);
     
 });
+$('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
+
+  // menu stuff
+
+  $(document).ready(function() {
+    $('#menu-toggle, #menu > ul > li > a').click(function(e) { 
+        var $toggle = $(this); 
+        var $menu = $('#' + $(this).attr('aria-controls')); 
+    
+        if ($menu.attr('aria-hidden') == 'true') {
+            $('body').addClass('open');
+            $menu.attr('aria-hidden', 'false');
+            $toggle.attr('aria-expanded', 'true');
+        }
+        else if ($menu.attr('aria-hidden') == 'false') {
+            $('body').removeClass('open');
+            $menu.attr('aria-hidden', 'true');
+            $toggle.attr('aria-expanded', 'faremove'); 
+        }
+    });
+    // Set button to click.
+var button = document.getElementById( 'menu-toggle' );
+
+// Click the button.
+button.onclick = function() {
+	
+  // Toggle class "opened". Set also aria-expanded to true or false.
+  if ( -1 !== button.className.indexOf( 'opened' ) ) {
+    button.className = button.className.replace( ' opened', '' );
+    button.setAttribute( 'aria-expanded', 'false' );
+  } else {
+     button.className += ' opened';
+    button.setAttribute( 'aria-expanded', 'true' );
+   }
+    
+ };
+});
+
